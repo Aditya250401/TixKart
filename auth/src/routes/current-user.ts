d0d7f354537a36158f296b50991295cdb3221113
Aditya250401 <aa.21u10091@btech.nitdgp.ac.in>
2024-09-20 12:02:49 +0530

    kjshdvbhbj

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -4,6 +4,7 @@ import { currentUser } from '@aditya250401/common'
 const router = express.Router()
 
 router.get('/api/users/currentuser', currentUser, (req, res) => {
+	console.log('things went alright')
 	res.send({ currentUser: req.currentUser || null })
 })
 
